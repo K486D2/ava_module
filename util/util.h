67d46414f10a3554ce7a9314df0eb82911152c97
huigang39 <huigang39@outlook.com>
2025-09-26 20:22:52 +0800

    feat

--- util/util.h
+++ util/util.h
@@ -5,7 +5,7 @@
 #include "def.h"
 #include "printutil.h"
 
-#define AT(x) __attribute__((section(x)))
+#define AT(addr) __attribute__((section(addr)))
 
 #define ATOMIC_EXEC(code)                                                                          \
   do {                                                                                             \
@@ -15,11 +15,11 @@
     __set_PRIMASK(_primask);                                                                       \
   } while (0)
 
-#define ARG_UNUSED(x) (void)(x)
+#define ARG_UNUSED(arg) (void)(arg)
 
-#define ARG_CHECK(x)                                                                               \
+#define ARG_CHECK(arg)                                                                             \
   do {                                                                                             \
-    if ((x) == NULL)                                                                               \
+    if ((arg) == NULL)                                                                             \
       return -MEINVAL;                                                                             \
   } while (0)
 
@@ -29,12 +29,12 @@
       f_init((p), (p)->cfg);                                                                       \
   } while (0)
 
+#define RESET_OUT(ptr)       memset(&(ptr)->out, 0, sizeof((ptr)->out))
+
 #define BUILD_BUG_ON_ZERO(e) (sizeof(struct { int : -!!(e); }))
 #define __same_type(a, b)    __builtin_types_compatible_p(typeof(a), typeof(b))
 #define __must_be_array(a)   BUILD_BUG_ON_ZERO(__same_type((a), &(a)[0]))
 #define ARRAY_SIZE(arr)      (sizeof(arr) / sizeof(arr[0]) + __must_be_array(arr))
-
-#define RESET_OUT(ptr)       memset(&(ptr)->out, 0, sizeof((ptr)->out))
 
 static inline void swap_byte_order(void *data, const u32 size) {
   u32 *p = (u32 *)data;
