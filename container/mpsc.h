98c0ee94e763b4490b25f18f48834244fc158101
huigang39 <huigang39@outlook.com>
2025-09-29 21:42:04 +0800

    feat

--- container/mpsc.h
+++ container/mpsc.h
@@ -12,7 +12,7 @@
 
 typedef struct {
   size_t seen_off;
-  size_t id;
+  size_t registered;
 } mpsc_producer_t;
 
 typedef struct {
@@ -27,15 +27,27 @@ typedef struct {
 static inline void
 mpsc_init(mpsc_t *mpsc, size_t cap, mpsc_producer_t *producers, size_t nproducers) {
   mpsc->cap        = cap;
-  mpsc->next       = 0;
   mpsc->end        = MPSC_OFF_MAX;
-  mpsc->written    = 0;
   mpsc->nproducers = nproducers;
   mpsc->producers  = producers;
-  for (size_t i = 0; i < nproducers; i++) {
-    producers[i].seen_off = 0;
-    producers[i].id       = i;
-  }
+}
+
+void mpsc_get_sizes(size_t nworkers, size_t *ringbuf_size, size_t *ringbuf_worker_size) {
+  if (ringbuf_size)
+    // *ringbuf_size = offsetof(mpsc_t, workers[nworkers]);
+    if (ringbuf_worker_size)
+      *ringbuf_worker_size = sizeof(mpsc_producer_t);
+}
+
+mpsc_producer_t *mpsc_register(mpsc_t *rbuf, size_t i) {
+  mpsc_producer_t *w = &rbuf->producers[i];
+  w->seen_off        = MPSC_OFF_MAX;
+  atomic_store_explicit(&w->registered, true, memory_order_release);
+  return w;
+}
+
+void mpsc_unregister(mpsc_t *rbuf, mpsc_producer_t *w) {
+  w->registered = false;
 }
 
 #endif // !MPSC_H
